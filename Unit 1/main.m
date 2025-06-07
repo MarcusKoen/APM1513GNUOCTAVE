3a34d38560e17feecb32c11021272d5a313e2165
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-06-07 18:36:10 +0200

    Update main.m

--- Unit 1/main.m
+++ Unit 1/main.m
@@ -5,3 +5,20 @@
 A=[1 2;3 4]
 b=[5;6]
 x=A\b
+
+%Use the program to plot a simple graph: sin(x) in the range 0<x<7.5
+fplot(@sin,[0,7.5])
+
+%How to find information about a function?
+doc <fn>
+doc fplot
+
+%For documenting session
+diary on
+diary off
+
+%There is a complete user manual available in both html- and pdf-formats, located
+%at:
+%C:\Program Files\Octave\doc\HTML\liboctave\index.html
+%C:\Program Files\Octave\doc\HTML\interpreter\index.html
+%C:\Program Files\Octave\doc\PDF\octave.pdf
