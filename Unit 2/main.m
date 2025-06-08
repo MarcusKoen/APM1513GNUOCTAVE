c3852305eaf4db990f61233ad509e4f83671ade7
MarcusKoen <37168149+MarcusKoen@users.noreply.github.com>
2025-06-08 13:32:03 +0200

    Update main.m

--- Unit 2/main.m
+++ Unit 2/main.m
@@ -1,1 +1,14 @@
+%Variable assignments
+%Variables must start with a letter and are case-sensitive, can include numbers and underscore
+%Some pre-defined variables
+e
+i
+j
+pi
+a=2
+%can use the who command to check user-defined variables
+who
 
+%remove all definitions by using the clear command
+clear
+%Good practice after solving a problem i.o. quitting
